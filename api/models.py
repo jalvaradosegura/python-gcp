71b94e965969e7d62a133c0a46bf649fcd7311d1
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2020-08-22 00:33:59 -0400

    - Create Drug and Vaccination models - Add a basic tests for testing the models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Drug(models.Model):
+    name = models.TextField()
+    code = models.CharField(max_length=10, unique=True)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Vaccination(models.Model):
+    rut = models.TextField()
+    dose = models.FloatField(
+        validators=[MinValueValidator(0.15), MaxValueValidator(1.0)]
+    )
+    date = models.DateField(auto_now_add=True)
+    drug = models.ForeignKey(
+        Drug,
+        related_name='vaccinations',
+        on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return f'Vaccination for {self.rut}'
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Drug, Vaccination
+
+
+class DrugModelTests(TestCase):
+
+    def setUp(self):
+        self.drug = Drug.objects.create(
+            name='test drug',
+            code='test code',
+            description='test description'
+        )
+
+    def test_drug_str(self):
+        self.assertEqual(self.drug.__str__(), 'test drug')
+
+
+class VaccinationModelTests(TestCase):
+
+    def setUp(self):
+        drug = Drug.objects.create(
+            name='test drug',
+            code='test code',
+            description='test description'
+        )
+        self.vaccination = Vaccination.objects.create(
+            rut='11.111.111-1',
+            dose=0.5,
+            drug=drug
+        )
+
+    def test_vaccination_str(self):
+        self.assertEqual(
+            self.vaccination.__str__(),
+            f'Vaccination for {self.vaccination.rut}'
+        )
