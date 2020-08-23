188d73f0b63a698b7ed4f60b06201d981803ed2f
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2020-08-23 02:23:45 -0400

    Move ALLOWED_HOSTS settings to an environment variable

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
+if ALLOWED_HOSTS_ENV:
+    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
 
 # Application definition
 
