b5437c95073805870a431d5c52033e0126a304e5
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2020-08-23 16:13:03 -0400

    - Make the docker-compose.yml public, now use .env - Make a setup.sh file that install everything this project needs to work in a Ubuntu environment

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Run with: yes Y | sh entry_point.sh
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
+sudo apt update
+sudo apt install docker-ce
+# Install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-s)-$(uname -m)" -o /usr/loc
+al/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o
+ /usr/local/bin/docker-compose
+# give permissions to it
+sudo chmod +x /usr/local/bin/docker-compose
+# Download the project
+sudo git clone https://github.com/jalvaradosegura/python-gcp.git
+# exec bash
+cd python-gcp/
+# Execute containers
+sudo docker-compose up -d --build
