4a8e0c456c5ddd0fbc7ba1673e0467d3f011afee
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2020-08-23 19:00:16 -0400

    Adjust the setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-# Run with: yes Y | sh entry_point.sh
+# Run this is script with: yes Y | sh setup.sh
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
 sudo apt update
 sudo apt install docker-ce
 # Install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-s)-$(uname -m)" -o /usr/loc
-al/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o
- /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # give permissions to it
 sudo chmod +x /usr/local/bin/docker-compose
 # Download the project
 sudo git clone https://github.com/jalvaradosegura/python-gcp.git
-# exec bash
+
+# Environment file
+ENVFILE=.env
+
+# Move it to the project folder
+sudo mv .env python-gcp/
+
 cd python-gcp/
+
 # Execute containers
 sudo docker-compose up -d --build
+
+# Migrate the database
+sudo docker-compose exec web python manage.py migrate
